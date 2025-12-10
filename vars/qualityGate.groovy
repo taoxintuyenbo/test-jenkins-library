3d4a0cf4e2896296a5c7d47aaccda374fa455446
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-10 14:01:08 +0700

    Create qualityGate.groovy

---
+++ vars/qualityGate.groovy
@@ -0,0 +1,10 @@
+def call() {
+  stage('Quality Gate'){
+    timeout(time: 1, unit: 'HOURS') {
+        def qg = waitForQualityGate()
+        if (qg.status != 'OK') {
+            error "Pipeline aborted due to quality gate failure: ${qg.status}"
+        }
+    }
+  }
+}
