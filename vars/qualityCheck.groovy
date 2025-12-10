6cb778be662b154ab241441d26055d0f8a6da69d
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-10 14:17:23 +0700

    Update qualityCheck.groovy

--- vars/qualityCheck.groovy
+++ vars/qualityCheck.groovy
@@ -1,7 +1,7 @@
 def call () {
   stage ('Check with SonarQube') {
     withSonarQubeEnv(env.SONARQUBE_SERVER){
-      sh "${MAVEN_HOME}/bin/mvn sonar:sonar -Dsonar.projectName=${PROJECT_NAME}-${BRANCH_NAME} -Dsonar.projectKey=${PROJECT_NAME}-${BRANCH_NAME}"
+      sh "${MAVEN_HOME}/bin/mvn sonar:sonar -Dsonar.projectName=${PROJECT_NAME}-${env.BRANCH_NAME} -Dsonar.projectKey=${PROJECT_NAME}-${env.BRANCH_NAME}"
     }
   }
 }
